6c17aded139fd922af6978974dada2cc1c1e886f
Muyedur rahman <muyedrahman732@gmail.com>
2026-02-04 18:14:45 +0600

    Add optional chaining and dot vs bracket notation

---
+++ chain.js
@@ -0,0 +1,14 @@
+const student = {
+  name: "Rafid",
+  1: 50,
+  family: {
+    title: 'bhuyiah',
+    mother: {
+        name: 'noor jahan',
+        age: 45,
+    }
+  },
+  marks: 90,
+};
+// console.log(student.family.title)
+console.log(student.family.mother?.age)
---
+++ dot-bracket.js
@@ -0,0 +1,11 @@
+const student = {
+    name: 'Rafid',
+    1:50,
+    marks: 90
+}
+
+// dot notation
+const studentName = student.name;
+
+// bracket notation
+const studentName2 = student ['name']
