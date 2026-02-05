c2b0b40fae0d52084a0a637a2ece1a8bf43fd02b
Muyedur rahman <muyedrahman732@gmail.com>
2026-02-05 21:18:57 +0600

    Practice this keyword in class, regular function and arrow function

---
+++ this.js
@@ -0,0 +1,34 @@
+class Vehicle{
+    constructor(type,brand ,price){
+        this.type = type;
+        this.brand = brand;
+        this.price = price;
+    }
+    getThis(){
+        console.log(this)
+    }
+    getPrice (){
+        return this.price
+    }
+}
+const car1 = new Vehicle('car','Honda',500000);
+const car2 = new Vehicle("car", "Toyota", 400000);
+// car1.getThis();
+// car2.getPrice();
+
+// console.log(car2.getPrice())
+// console.log(car1.getPrice())
+
+const student = {
+    name: 'Rahul dravid',
+    score: 10000,
+    getScore: function(){
+        console.log(this)
+    },
+    getScoreArrow: ()=>{
+        console.log(this)
+    }
+}
+student.getScore();
+student.getScoreArrow();
+console.log('ultimate thix', this)
